b17701628f912d2198d41a799b437488dec319c2
Daniil-Myalo <74824230+Daniil-Myalo@users.noreply.github.com>
2025-09-10 23:35:03 +0300

    Add files via upload

---
+++ JS/lang.js
@@ -0,0 +1,43 @@
+const langArr = {
+    "until" : {
+        "ru": "Онлайн-тюнер для барабанов",
+        "ua": "Онлайн-тюнер для барабанів",
+        "en": "Online drum tuner",
+    },
+    "title" : {
+        "ru": " Тюнер для барабанов",
+        "ua": " Тюнер для барабанів",
+        "en": " Drum tuner",
+    },
+    "button_start" : {
+        "ru": "Начать",
+        "ua": "Почати",
+        "en": "Begin",
+    },
+    "button_end" : {
+        "ru": "Отмена",
+        "ua": "Відміна",
+        "en": "Cancellation",
+    },
+    "frequency" : {
+        "ru": "Целевая частота ",
+        "ua": "Цільова частота ",
+        "en": "Target frequency ",
+    },
+    "sensitivity" : {
+        "ru": "Чувствительность микрофона",
+        "ua": "Чутливість мікрофона",
+        "en": "Microphone sensitivity",
+    },
+    "description" : {
+        "ru": "Выбери лаг, ударь рядом с ним палочкой и подстрой. Цель: все лаги на одной частоте (введи цель выше). Теперь показывает ноту и позволяет регулировать чувствительность.",
+        "ua": "Обери лаг, вдар поряд з ним палочкою та підстрой. Ціль: всі лаги на одной частоте (введи цель выше). Теперь показывает ноту и позволяет регулировать чувствительность.",
+        "en": "Pick a lag, hit next to it with your wand and tweak it. Target: all lags on the same frequency (enter target above). Now shows the note and allows you to adjust the sensitivity.",
+    },
+    "language" : {
+        "ru": "Язык",
+        "ua": "Мова",
+        "en": "Language",
+    }
+
+}
